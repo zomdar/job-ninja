b69ec490e36589be1df3c3874a9064899b8a404e
Wooj C <zomdar@gmail.com>
2023-05-02 21:44:45 -0400

    adding navbar and routing

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,17 +1,34 @@
 // src/NavBar.tsx
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="bg-draculaBackground p-4 shadow-md">
-      <div className="container mx-auto flex items-center">
-      <img
-          src={`${process.env.PUBLIC_URL}/images/ninja_logo.png`}
-          alt="Ninja Logo"
-          className="h-10 w-10"
-        />
-        <span className="text-draculaForeground text-xl font-bold ml-4">Job Ninja</span>
+    <nav className="bg-draculaSelection py-4">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <Link to="/">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/ninja_logo.png`}
+            alt="Ninja Logo"
+            className="w-10 h-10"
+          />
+        </Link>
+        <ul className="flex">
+          <li className="mx-2">
+            <Link to="/" className="text-draculaForeground font-semibold">
+              Home
+            </Link>
+          </li>
+          <li className="mx-2">
+            <Link
+              to="/other-page"
+              className="text-draculaForeground font-semibold"
+            >
+              Compose
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
